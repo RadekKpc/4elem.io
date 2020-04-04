d964d7a5b3350cbcef4313110d79cbfa40e785ca
Radekk814 <radek1198@o2.pl>
2020-04-04 16:51:34 +0200

    usuniecie folderu DataStructures

--- DataStructures/Player.py
+++
@@ -1,8 +0,0 @@
-# narazie nie wiem co tu dopisac najpierw serwer trzeba napisac
-class Player:
-
-    def __init__(self,x_position,y_position,radius,id,name):
-        self.position = (x_position,y_position)
-        self.radius = radius
-        self.id = id
-        self.name = name
